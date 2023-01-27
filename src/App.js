7fd1e5d98250c04757a763872d87a48dc089c1e3
ROHITchin995 <rohitsure.9915@gmail.com>
2023-01-27 16:05:11 +0530

    Follower page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { FetchUsers } from './FetchUsers';
 
 function App() {
   const {loading, data} = FetchUsers()
+  const [page, setPage] = useState(0)
+  const [followers, setFollowers] = useState([])
+
+  useEffect(()=>{
+    if(loading) return
+    setFollowers(data[page])
+  },[loading, page])
+
   return (
-   <>
-   
-   </>
+   <main>
+    <div className='section-title'>
+        <h1>{loading ? 'loading...' : 'Profiles'}</h1>
+        <div className='underline'></div>
+      </div>
+   </main>
   )
 }
 
