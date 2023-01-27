ec772af9fa079cf337889fd9d267603ebd772c6a
ROHITchin995 <rohitsure.9915@gmail.com>
2023-01-27 16:00:44 +0530

    fetch users done

---
+++ src/FetchUsers.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+
+const url = 'https://api.github.com/users/fabpot/followers?per_page=100'
+
+export const FetchUsers = () => {
+    const [loading, setLoading] = useState(true)
+    const [data, setData] = useState([])
+
+    const getProfiles = async ()=>{
+        const response = await fetch(url)
+        const data = await response.json()
+        setData(data)
+        setLoading(false)
+    }
+    useEffect(() => {
+      getProfiles()
+    
+    }, [])
+    
+  return { loading, data }
+}
