136d3ef54af4573c638fea0458719eaacb82721e
ROHITchin995 <rohitsure.9915@gmail.com>
2023-01-27 16:08:31 +0530

    home page and pagination done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { FetchUsers } from './FetchUsers';
-
+import Follower from './Follower'
 function App() {
   const {loading, data} = FetchUsers()
   const [page, setPage] = useState(0)
   const [followers, setFollowers] = useState([])
-
+ 
   useEffect(()=>{
     if(loading) return
     setFollowers(data[page])
   },[loading, page])
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1
+      if (nextPage > data.length - 1) {
+        nextPage = 0
+      }
+      return nextPage
+    })
+  }
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1
+      if (prevPage < 0) {
+        prevPage = data.length - 1
+      }
+      return prevPage
+    })
+  }
+
+  const handlePage = (index) => {
+    setPage(index)
+  }
 
   return (
    <main>
@@ -18,6 +40,34 @@ function App() {
         <h1>{loading ? 'loading...' : 'Profiles'}</h1>
         <div className='underline'></div>
       </div>
+      <section className='followers'>
+        <div className='container'>
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />
+          })}
+        </div>
+        {!loading && (
+          <div className='btn-container'>
+            <button className='prev-btn' onClick={prevPage}>
+              prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page ? 'active-btn' : null}`}
+                  onClick={() => handlePage(index)}
+                >
+                  {index + 1}
+                </button>
+              )
+            })}
+            <button className='next-btn' onClick={nextPage}>
+              next
+            </button>
+          </div>
+        )}
+      </section>
    </main>
   )
 }
